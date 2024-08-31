1f1fed5460d5073a675de51c0195d241c2eb7b51
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2024-08-31 22:42:48 +0530

    Time: 19 ms (43.71%), Space: 43.8 MB (74.46%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,40 +1,45 @@
-class Solution {
+public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> resultSet = new HashSet<>(); 
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
         
-        Arrays.sort(nums); 
+        Arrays.sort(nums);  // Sort the array first
+        int n = nums.length;
         
-        for (int i = 0; i < nums.length - 3; i++) {
-            for (int j = i + 1; j < nums.length - 2; j++) {
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates for the second element
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
                 int left = j + 1;
-                int right = nums.length - 1;
+                int right = n - 1;
                 
                 while (left < right) {
-                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    
                     if (sum == target) {
-                        List<Integer> arr = new ArrayList<>();
-                        arr.add(nums[i]);
-                        arr.add(nums[j]);
-                        arr.add(nums[left]);
-                        arr.add(nums[right]);
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         
-                        if (!resultSet.contains(arr)) {
-                            resultSet.add(arr); 
-                        }
+                        // Skip duplicates for the third element
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // Skip duplicates for the fourth element
+                        while (left < right && nums[right] == nums[right - 1]) right--;
                         
                         left++;
                         right--;
-                    } else if (sum <= target) {
+                    } else if (sum < target) {
                         left++;
-                    } else if(sum > target){
+                    } else {
                         right--;
                     }
                 }
             }
         }
-        
-        result.addAll(resultSet); 
         
         return result;
     }
