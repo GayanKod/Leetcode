28c504f3a6aa6ecb8c795559f27c42be29b19473
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2024-09-03 19:42:16 +0530

    Time: 2 ms (94.63%), Space: 46.7 MB (36.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,14 +3,7 @@ class Solution {
         
         int[] arr = new int[n+1];
 
-        arr[0] = 0;
-        if (n==0) return arr;
-        arr[1] = 1;
-        if (n==1) return arr;
-        arr[2] = 1;
-        if (n==2) return arr;
-
-        for (int i=3; i<=n; i++){
+        for (int i=0; i<=n; i++){
             if (i%2 == 0){
                 arr[i] = arr[i/2];
             }else{
